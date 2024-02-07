45894b13472048ba16b09e0043ba118f439ca9f3
safsalem <safsalem123456@gmail.com>
2024-02-07 15:47:30 +0300

    task for lab

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,30 @@
 public class Sorter {
     public static void main(String[] args) {
-        System.out.println("Sorter");
+        int[] Array = new int[7];
+        for (int i = 0; i < Array.length; i++) {
+            Array[i] = (int) (Math.random() * 100);
+        }
+        for(int x: Array){
+            System.out.print(x + " ");
+        }
+        selectionSort(Array);
+    }
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int index = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[index]) {
+                    index = j;//searching for lowest index
+                }
+            }
+            int smallerNumber = arr[index];
+            arr[index] = arr[i];
+            arr[i] = smallerNumber;
+        }
+        System.out.println("\nafter sort");
+        for(int x: arr){
+            System.out.print(x + " ");
+        }
     }
 }
