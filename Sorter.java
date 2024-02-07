36edf6ca102dba48710e24fe64c8521ab92cef26
ARYAM AHMED ALSHEHRI <s202157070@kfupm.edu.sa>
2024-02-07 16:06:32 +0300

    add print statement

--- Sorter.java
+++ Sorter.java
@@ -27,4 +27,17 @@ public class Sorter {
             System.out.print(x + " ");
         }
     }
+    public void insertionSort(int[] array){
+        int n = array.length;
+        for(int i = 1;i<n ;++i){
+            int key = array[i];
+            int j=i-1;
+
+            while(j>= 0 && array[j]> key){
+                array[j+1]= array[j];
+                j=j-1;
+            }
+            array[j+1]=key;
+            }
+        }
 }
